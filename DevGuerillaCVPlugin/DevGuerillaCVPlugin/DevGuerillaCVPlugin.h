1907acc469edb0fc9713fb4c0e9f63597e2aea2f
Leonid Lazaryev <leonidlazaryev@gmail.com>
2019-04-29 15:54:50 +0200

    plugin solution sources

---
+++ DevGuerillaCVPlugin/DevGuerillaCVPlugin/DevGuerillaCVPlugin.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+// Use this macros just to keep track of arguments we are using
+#define _IN_
+// Use this macros just to keep track of arguments we are using
+#define _OUT_
+
+namespace CVPlugin
+{
+	// Our income frame data
+	typedef struct FRAME_DATA
+	{
+		int width;
+		int height;
+		int channels;
+		unsigned char * pixels;
+	} FrameData;
+
+	// Our first processing function
+	extern "C" void __declspec(dllexport) _stdcall
+	processFrameCanny(_IN_ FrameData *, _OUT_ unsigned char *);
+}
