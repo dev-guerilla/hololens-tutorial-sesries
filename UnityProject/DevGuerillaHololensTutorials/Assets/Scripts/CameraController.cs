ef569b76185fdc0fde07437c3da6a92a14dc662b
Leonid Lazaryev <leonidlazaryev@gmail.com>
2019-04-29 15:47:53 +0200

    unity project add

---
+++ UnityProject/DevGuerillaHololensTutorials/Assets/Scripts/CameraController.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    WebCamTexture webcamTexture;
+    // color buffer to reduce GC overhead
+    Color32[] buffer = null;
+
+
+    MaterialPropertyBlock prop;
+    public MeshRenderer visualizeCapture;
+
+    // Basic getters, should be self explanatory
+    public bool IsPlaying
+    {
+        get
+        {
+            return webcamTexture && webcamTexture.isPlaying;
+        }
+    }
+
+    public int Width
+    {
+        get
+        {
+            return 896;
+        }
+    }
+
+    public int Height
+    {
+        get
+        {
+            return 504;
+        }
+    }
+
+    public Color32[] Pixels {
+        get
+        {
+            if (webcamTexture)
+                webcamTexture.GetPixels32(buffer);
+            return buffer;
+        }
+    }
+
+    public static CameraController Instance 
+    {
+        get; private set;
+    }
+
+    void CreateTexture()
+    {
+        // Create Unity webcam texture
+        webcamTexture = new WebCamTexture(Width, Height, 30);
+        // Init buffer
+        buffer = new Color32[Width * Height];
+        // Start camera stream
+        webcamTexture.Play();
+        if (visualizeCapture)
+        {
+            prop = new MaterialPropertyBlock();
+            prop.SetTexture("_MainTex", webcamTexture);
+            visualizeCapture.SetPropertyBlock(prop);
+        }
+    }
+
+    IEnumerator Start()
+    {
+        if (Instance)
+        {
+            Destroy(this);
+            yield break;
+        }
+        Instance = this;
+        FindWebCams();
+
+        // Let user agree to use the webcam first
+        // This call 
+        yield return Application.RequestUserAuthorization(UserAuthorization.WebCam);
+        if (Application.HasUserAuthorization(UserAuthorization.WebCam))
+        {
+            Debug.Log("webcam found");
+            CreateTexture();
+        }
+        else
+        {
+            Debug.Log("webcam not found");
+        }
+    }
+
+    void FindWebCams()
+    {
+        // Iterate over available devices
+        foreach (var device in WebCamTexture.devices)
+        {
+            Debug.Log("Name: " + device.name);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (webcamTexture)
+        {
+            webcamTexture.Stop();
+            Destroy(webcamTexture);
+            buffer = null;
+            Instance = null;
+        }
+    }
+}
---
+++ UnityProject/DevGuerillaHololensTutorials/Assets/Scripts/Compositor.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using UnityEngine;
+
+public class Compositor : MonoBehaviour
+{
+    // Buffer to hold processed data
+    byte[] processedPixelsBuffer;
+    // Buffer to hold raw data
+    byte[] rawPixelBufer;
+    // Texture for processed data
+    Texture2D processedTexture;
+   
+    bool processingReady = false;
+    bool shouldUpdateFrame = false;
+
+    public MeshRenderer visualizeProcessing;
+    MaterialPropertyBlock prop;
+
+    bool CreateTexture()
+    {
+        if (processedTexture)
+        {
+            return true;
+        }
+        if (CameraController.Instance && CameraController.Instance.IsPlaying)
+        {
+            var cc = CameraController.Instance;
+
+            // Prepare texture and buffer
+            processedTexture = new Texture2D(cc.Width, cc.Height, TextureFormat.Alpha8, false);
+            processedPixelsBuffer = new byte[cc.Width * cc.Height];
+            rawPixelBufer = new byte[cc.Width * cc.Height * 4];
+            processedTexture.Apply();
+            if (visualizeProcessing)
+            {
+                // Set texture to visualize processed data
+                prop = new MaterialPropertyBlock();
+                prop.SetTexture("_MainTex", processedTexture);
+                visualizeProcessing.SetPropertyBlock(prop);
+            }
+            return true;
+        }
+        return false;
+    }
+
+    void Update()
+    {
+        if (processingReady)
+        {
+            if (shouldUpdateFrame)
+            {
+                shouldUpdateFrame = false;
+                ProcessUpdateFrame();
+            }
+        }
+        else
+        {
+            if (processingReady = CreateTexture())
+            {
+                shouldUpdateFrame = true;
+            }
+        }
+    }
+
+    private void ProcessUpdateFrame()
+    {
+        var cc = CameraController.Instance;
+        if (!cc || !cc.IsPlaying)
+        {
+            Debug.LogError("Something went wrong, CameraController is not valid");
+            return;
+        }
+        Debug.Log("Updating frame");
+        var pix = cc.Pixels;
+        for (int i = 0; i < pix.Length; i++)
+        {
+            rawPixelBufer[i * 4] = pix[i].r;
+            rawPixelBufer[i * 4 + 1] = pix[i].g;
+            rawPixelBufer[i * 4 + 2] = pix[i].b;
+            rawPixelBufer[i * 4 + 3] = pix[i].a;
+        }
+        try
+        {
+            FrameData fd = new FrameData()
+            {
+                width = cc.Width,
+                height = cc.Height,
+                channels = 4, // Color32 struct has 4 channels (r,g,b,a)
+                pixels = rawPixelBufer
+            };
+            // Call plugin function
+            CVPlugin.ProcessFrame(ref fd, processedPixelsBuffer);
+            // Update texture
+            processedTexture.LoadRawTextureData(processedPixelsBuffer);
+            processedTexture.Apply();
+            shouldUpdateFrame = true;
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("Error during processing");
+            Debug.LogError(e.Message);
+        }
+        
+    }
+}
